f4b1b8bf4f75270d7586f47427812f0e806e6ea9
Sushain Kaushal <sushainkaushal@gmail.com>
2026-04-07 23:31:54 +0530

    motion added in hero section

--- src/components/Homepage/HeroSection.tsx
+++ src/components/Homepage/HeroSection.tsx
@@ -1,12 +1,22 @@
 
 
+import { useState,useEffect } from "react";
 
-
-
+const Counter = ({ target }: { target: number }) =>
+     { const [count, setCount] = useState(0); 
+        useEffect(() => { let start = 0; const duration = 2000; // 2 sec 
+        const increment = target / (duration / 16);
+         const timer = setInterval(() => { start += increment; 
+            if (start >= target) { setCount(target); 
+                clearInterval(timer); } 
+            else { setCount(Math.floor(start)); } }, 16);
+             return () => clearInterval(timer); }, [target]);
+              return <span>{count}</span>; };
+               
 
 export const HeroSection =()=>{   
 
-
+const [resetKey, setResetKey] = useState(0);
   return (
     <div className="relative h-[120vh] w-full overflow-hidden">
       
@@ -19,11 +29,45 @@ export const HeroSection =()=>{
       </div>
    </div>
     
-      <div className="relative flex h-full items-center justify-center text-white">
-        <h1 className="text-5xl font-bold">Wake up in Bergen</h1>
-      </div>
+      <div className="relative flex h-full items-center justify-center text-white animate-fadeUp">
+       <h1 className="text-4xl md:text-5xl font-semibold text-white leading-tight"
+    style={{ fontFamily: "Playfair Display, serif" }}
+>
+  Step into a space where comfort embraces you,
+  <br />
+  <span className="text-lg md:text-xl font-light"
+        style={{ fontFamily: "Inter, sans-serif" }}>
+    and every night feels like a peaceful escape.
+  </span>
+</h1>
+<div className="absolute bottom-10 w-full flex justify-center gap-10 text-white text-xl">
 
-      
+  <div  className="animate-fadeUp"
+    onAnimationIteration={() => {
+      setResetKey((prev) => prev + 1);
+    }}>
+    <Counter key={resetKey} target={500} />+
+    <p>Guests</p>
+  </div>
+
+  <div  className="animate-fadeUp"
+  onAnimationIteration={() => {
+      setResetKey((prev) => prev + 1);
+    }}>
+    <Counter key={resetKey} target={120} />+
+    <p>Rooms</p>
+  </div>
+
+  <div className="animate-fadeUp"
+   onAnimationIteration={() => {
+      setResetKey((prev) => prev + 1);
+    }}>
+    <Counter  key={resetKey} target={20} />+
+    <p>Awards</p>
+  </div>
+
+</div>
+      </div>
     </div>
   );
 
