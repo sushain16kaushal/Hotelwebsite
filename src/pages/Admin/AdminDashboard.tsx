df4f75f70bdcc86c51992d23d743484df4e8cc98
Sushain Kaushal <sushainkaushal@gmail.com>
2026-04-15 01:36:14 +0530

    code improved

--- src/pages/Admin/AdminDashboard.tsx
+++ src/pages/Admin/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import  { useEffect, useState } from 'react';
 import axios from 'axios';
-import type { Hotels } from '../../types/content';
+
 
 const AdminDashboard = () => {
   const [data, setData] = useState<any>(null);
