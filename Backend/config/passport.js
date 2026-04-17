792afab6eaaefe17f52390a864ff42aeafb9be63
Sushain Kaushal <sushainkaushal@gmail.com>
2026-04-17 17:25:49 +0530

    callback url updated

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -7,7 +7,7 @@ const passportConfig = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/api/auth/google/callback"
+        callbackURL: "https://hotelapp-tiof.onrender.com/api/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
         try {
